ce0a804e9f177a5f25a5b728aae57cc9e99ee6fb
marcusclayton <marcusaclayton@gmail.com>
2017-02-24 16:07:53 -0800

    update

--- Tests/script.tests.ps1
+++ Tests/script.tests.ps1
@@ -1,1 +1,14 @@
-###
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path ) -replace 'tests', 'code'
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+write "$here\$sut"
+. "$here\$sut"
+
+
+
+Describe "Running App Specific Pester Tests" {
+  context 'Context Block 1'{
+      It "does something useful" {
+          $true | Should Be $true
+      }
+    }
+}

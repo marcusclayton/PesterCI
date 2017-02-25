dbc0a4b097a0005b1192d5989f0e26e251dd748a
marcusclayton <marcusaclayton@gmail.com>
2017-02-24 16:58:09 -0800

    Add files via upload

---
+++ Tests/PSSA.tests.ps1
@@ -0,0 +1,22 @@
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path ) -replace 'tests', 'code'
+$file = "script.ps1"
+$script = "$here\$file"
+
+
+Describe 'Testing against PSSA rules' {
+       Context 'PSSA Standard Rules' {
+        $analysis = Invoke-ScriptAnalyzer -Path $script
+        $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+ 
+        forEach ($rule in $scriptAnalyzerRules) {
+            It "Should pass $rule" {
+                If ($analysis.RuleName -contains $rule) {
+                    $analysis |
+                         Where RuleName -EQ $rule -outvariable failures |
+                         Out-Default
+                    $failures.Count | Should Be 0
+                }
+            }
+        }
+    }
+}

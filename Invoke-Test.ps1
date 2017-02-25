00bf16d90d533983baf15f4112722a2cfe618458
marcusclayton <marcusaclayton@gmail.com>
2017-02-24 16:16:53 -0800

    Workaround Install-Module
    Working around issue with Pester Module version check, https://github.com/pester/Pester/issues/585

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'stop'
 Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
-Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false
-Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
+Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false -SkipPublisherCheck
+Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false -SkipPublisherCheck
 Import-Module Pester
 Import-Module PSScriptAnalyzer
 Invoke-Pester -OutputFile '.\Results\PesterResults.xml' -OutputFormat 'NUnitXml' -Script '.\Tests\Set-E.tests.ps1'

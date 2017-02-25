65f9a34f6d9b8eba707dcdcc9fb9a96a3002541c
marcusclayton <marcusaclayton@gmail.com>
2017-02-24 16:20:01 -0800

    update pester test path

--- Invoke-Test.ps1
+++ Invoke-Test.ps1
@@ -4,5 +4,5 @@ Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false -SkipPubli
 Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false -SkipPublisherCheck
 Import-Module Pester
 Import-Module PSScriptAnalyzer
-Invoke-Pester -OutputFile '.\Results\PesterResults.xml' -OutputFormat 'NUnitXml' -Script '.\Tests\Set-E.tests.ps1'
+Invoke-Pester -OutputFile '.\Results\PesterResults.xml' -OutputFormat 'NUnitXml' -Script '.\Tests\script.tests.ps1'
 Invoke-Pester -OutputFile '.\Results\PSSAResults.xml' -OutputFormat 'NUnitXml' -Script '.\Tests\PSSA.tests.ps1'

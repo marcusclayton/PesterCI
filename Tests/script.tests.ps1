be5d5137525d6bd4679aaccb37ad2d2b672f378f
marcusclayton <marcusaclayton@gmail.com>
2017-02-24 16:49:19 -0800

    Update script.tests.ps1

--- Tests/script.tests.ps1
+++ Tests/script.tests.ps1
@@ -1,7 +1,13 @@
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path ) -replace 'tests', 'code'
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+write "here"
+write $here
+write "sut"
+write $sut
+get-childitem $here
+write "here\sut"
 write "$here\$sut"
-. "$here\$sut"
+#. "$here\$sut"
 
 
 
